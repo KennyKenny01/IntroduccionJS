1ce364bb79c70c8d1a6fc3764c883463938425d7
KennyF <Kennedymiguel06@gmail.com>
2023-06-13 00:56:46 -0400

    Declarando variables
    Declarando variables con {var}

---
+++ js/01.js
@@ -0,0 +1,24 @@
+//Variables
+
+var producto = 'Audifonos Gamer'; //Crear variable y asignar valor. 
+
+var disponibles; //Creamos la variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponibles = true; //Reasignando el valor de la variable
+
+var producto1 = 'Computadora',
+    disponible1 = true,         //Declarando varias variables y reasigando el valor utilizando un solo {var}
+    categoria = 'Computadoras';
+
+// var 1disponible;
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; //Pascal case
+var nombreproducto = 'Monitor HD';
+
+console.log (producto)
