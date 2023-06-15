2bd68fa1d94d012802c34173b6911a471465bdb6
KennyF <Kennedymiguel06@gmail.com>
2023-06-15 00:51:03 -0400

    Metodos para string

--- js/04.js
+++ js/04.js
@@ -1,4 +1,16 @@
 //String o cadenas de texto
-const producto = "Monitore de 20\"";
+const tweet = "Aprendoendo JavaScript con el curso de udemy";
+const producto2 = 'Monitor HD';
+const Email = 'Correo@correo.com';
+//length es par ala extension
+console.log(producto2);
+console.log(tweet.length);
 
-console.log(typeof producto)
+// IndexOF 
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'));
+console.log(Email.indexOf('@'));
+
+//Include (Retorna true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('Tablet'))
