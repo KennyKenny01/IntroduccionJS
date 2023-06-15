a0676698fe8180053022d07146dc3f35326a9484
KennyF <Kennedymiguel06@gmail.com>
2023-06-15 00:32:53 -0400

    let y const
    aprendiendo  tipos de datos

---
+++ js/02.js
@@ -0,0 +1,23 @@
+//Variables con let
+
+let producto = 'Audifonos Gamer'; //Crear variable y asignar valor. 
+
+let disponibles; //Creamos la variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponibles = true; //Reasignando el valor de la variable
+
+let producto1 = 'Computadora',
+    disponible1 = true,         //Declarando varias variables y reasigando el valor utilizando un solo {let}
+    categoria = 'Computadoras';
+
+// let 1disponible;
+let disponible_;
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; //Camelcase
+let NombreProducto = 'Monitor HD'; //Pascal case
+let nombreproducto = 'Monitor HD';
+
---
+++ js/03.js
@@ -0,0 +1,23 @@
+//Variables con const
+
+const producto = 'Audifonos Gamer'; //Crear variable y asignar valor. 
+
+const disponibles = true; //Creamos la variable sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+disponibles = true; //Reasignando el valor de la variable
+
+const producto1 = 'Computadora',
+    disponible1 = true,         //Declarando varias variables y reasigando el valor utilizando un solo {const}
+    categoria = 'Computadoras';
+
+// const 1disponible;
+const disponible_ = true;
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; //Camelcase
+const NombreProducto = 'Monitor HD'; //Pascal case
+const nombreproducto = 'Monitor HD';
+
