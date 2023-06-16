2d50a0003837817fbf6f13fef7fb573f9cb23e6d
KennyF <Kennedymiguel06@gmail.com>
2023-06-16 02:05:29 -0400

    Boolean
    Si o NO

---
+++ js/09.js
@@ -0,0 +1,15 @@
+//Booleans es un si o no 
+
+const boolean1 = true;
+const boolean2 = false;
+//const boolean3 = "True";
+
+
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+
+
+const boolean3 = new Boolean(true);
+
+console.log(typeof boolean3);
