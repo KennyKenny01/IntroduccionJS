4ce34c07c6b9361784ff02591f83413f701a909b
KennyF <Kennedymiguel06@gmail.com>
2023-06-16 01:49:22 -0400

    Orden de las operaciones
    Misma matemáticas  de escuela y los incremento toca repasarlo

---
+++ js/07.js
@@ -0,0 +1,20 @@
+// Orden de las operaciones 
+
+let resultado;
+resultado = (20 + 30) * 2; //igual que la escuela, primero se multiplica y luego se suma. Para que se suma primero se agrega parentisi a la que quieres que sea de primero
+resultado = (100 + 200 + 300) *.2; // Ejemplo para sacar porciento (Se usa para sacar impuesto de totales y para aplicar descuentos y mas cosas aparte del monto total)
+
+//console.log(resultado);
+
+// Incrementos
+
+
+let puntaje = 10;
+//Incremento en 1 ++
+puntaje++; //10
+++puntaje; // 12
+
+puntaje += 10; 
+
+
+console.log(puntaje);
