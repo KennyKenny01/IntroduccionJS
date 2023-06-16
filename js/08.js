7e8d6952344e0a011a2d0b804424d7c5ece12984
KennyF <Kennedymiguel06@gmail.com>
2023-06-16 01:57:45 -0400

    Concatenaciones y Template String

---
+++ js/08.js
@@ -0,0 +1,9 @@
+// Concatenacion 
+
+const nombre = 'Kenny';
+const email = 'Kenny@Kenny.com';
+
+console.log("Nombre Cliente: " + nombre + " Email: " + email); //Forma antigua de hacerlo, ahora se utiliza el Template String 
+
+//Template String - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`); //Forma mas facil, rapida y con menos codigos
