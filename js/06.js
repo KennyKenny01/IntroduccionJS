59c86bd4f550f98815177a03016fe0953c751a36
KennyF <Kennedymiguel06@gmail.com>
2023-06-16 01:37:08 -0400

    Funcion Math
    Math.'Funcion de Math'

---
+++ js/06.js
@@ -0,0 +1,16 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.1); //Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.9); //Floor siempre redondea hacia abajo
+resultado = Math.sqrt(144); // Para sacar raiz cuadrada
+resultado = Math.abs(-100); //Cambia de negativo a posito 
+resultado = Math.min(3,5,1,8,2,10,15); //Para mostrar el minimo
+resultado = Math.max(3,5,1,8,2,10,15); //Para mostrar el maximo
+resultado = Math.random(); // Numero random
+resultado = Math.floor (Math.random() * 30) //Dame un numero random que este por debajo de 30 
+
+console.log (resultado)
