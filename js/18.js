c02bae03b2e8fc824072050d6716f78119ac9ee7
KennyF <Kennedymiguel06@gmail.com>
2023-10-26 18:52:52 -0400

    metodos

---
+++ js/18.js
@@ -0,0 +1,19 @@
+function sumar (numero1, numero2) { //numero 1 y 2 son parametros
+    console.log(numero1 * numero2);
+
+}
+
+sumar(10, 10); //Argumentos o numeros reales
+sumar(10, 5); 
+sumar(10, 11); 
+sumar(10, 2); 
+sumar(10, 8); 
+
+
+//Expresion de la funcion
+const sumar2 = function(a,b){
+    console.log(a+b);
+
+}
+
+sumar2(2,5);
