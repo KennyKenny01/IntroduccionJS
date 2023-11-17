89dfd7d61a17470b62a76d4e223415e39937f7d2
KennyF <Kennedymiguel06@gmail.com>
2023-11-17 12:17:34 -0400

    Volviendo

---
+++ js/24.js
@@ -0,0 +1,36 @@
+
+
+////For Loop
+//for( let i = 0; i < 10; i++ ) {
+//    console.log(i);
+//}
+
+//for(let i = 1; i <= 100; i++){
+//    if( i % 2 === 0 ) {
+//        console.log(`El numero ${i} es PAR`)
+//    } else {
+//        console.log(`El numero ${i} es INPAR`)
+//    }
+//}
+
+//While Loop
+
+const carrito = [
+    { nombre: 'Monitor 20 Pulgada', Precio: 500},
+    { nombre: 'Laptop', Precio: 100},
+    { nombre: 'Nvidia GTX 3060', Precio:8500},
+    { nombre: 'Tablet', Precio: 400},
+    { nombre: 'Mouse Logitech Pro', Precio: 900},
+    { nombre: 'Teclado Mecanico', Precio: 700},
+    { nombre: 'MousePad', Precio: 100},
+    { nombre: 'Monitor 30 Pulgada', Precio: 800},
+    { nombre: 'Iphone 12 ProMax', Precio: 1200},
+];
+for(let i = 0; i < carrito.length; i++ ) {
+        console.log( carrito[i] );
+}
+
+
+
+
+//Do While Loop
